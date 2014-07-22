373a357a5d8494b0e4cf71283b641dd2e9a969c0
Koen Punt <koen@koenpunt.nl>
2014-07-22 21:11:30 +0200

    add AMD and CommonJS loader

--- build/jquery.countdown.js
+++ build/jquery.countdown.js
@@ -1,146 +1,157 @@
 (function() {
-  var Countdown,
-    __slice = [].slice;
+  var __slice = [].slice;
 
-  $.fn.extend({
-    countdown: function() {
-      var args, options;
-      options = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
-      return this.each(function() {
-        var countdown;
-        countdown = $(this).data('countdown');
-        if (countdown) {
-          if (countdown[options]) {
-            return countdown[options].apply(countdown, args);
-          }
-        } else {
-          return $(this).data('countdown', new Countdown(this, options));
-        }
+  (function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+      return define(['jquery', 'exports'], function($, exports) {
+        return factory(root, exports, $);
       });
+    } else if (typeof exports !== 'undefined') {
+      return factory(root, exports, require('jquery'));
+    } else {
+      return factory(root, {}, root.jQuery || root.$);
     }
-  });
-
-  Countdown = (function() {
-    Countdown.defaults = {
-      placeholder: '-',
-      duration: 1000,
-      digits: 8,
-      min: null,
-      max: null,
-      easing: function(x, t, b, c, d) {
-        return c * t / d + b;
-      }
-    };
-
-    function Countdown(elem, options) {
-      this.jq_elem = $(elem);
-      this.value = 0;
-      this.options = $.extend({}, Countdown.defaults, this.jq_elem.data(), options);
-      this.buildHtml();
-      this.parseOptions();
-    }
-
-    Countdown.prototype.parseOptions = function() {
-      if (this.options.number != null) {
-        this.setValue(this.options.number);
-      }
-      if (this.options.animateTo != null) {
-        return this.animateTo(this.options.animateTo);
-      }
-    };
-
-    Countdown.prototype.buildHtml = function() {
-      var digits, i;
-      i = 0;
-      digits = [];
-      while (i < this.options.digits) {
-        digits.push("<span class=\"digit digit-" + i + "\">" + this.options.placeholder + "</span>");
-        i++;
-      }
-      return this.jq_elem.append(digits.join("\n"));
-    };
-
-    Countdown.prototype.animateTo = function(value) {
-      var animationStep, difference, duration, startTime, time;
-      startTime = new Date().getTime();
-      time = 0;
-      if (typeof value === 'string') {
-        if (/^\d+$/.test(value)) {
-          this.targetValue = value;
-        } else {
-          this.targetValue = (this.targetValue || 0) + eval(value);
-        }
-      } else {
-        this.targetValue = value;
-      }
-      if ((this.options.max != null) && this.targetValue > this.options.max) {
-        this.jq_elem.trigger('countdown:limit', {
-          value: this.targetValue,
-          limit: 'max',
-          limited_to: this.options.max
-        });
-        this.targetValue = this.options.max;
-      }
-      if ((this.options.min != null) && this.targetValue < this.options.min) {
-        this.jq_elem.trigger('countdown:limit', {
-          value: this.targetValue,
-          limit: 'min',
-          limited_to: this.options.min
-        });
-        this.targetValue = this.options.min;
-      }
-      difference = this.targetValue - this.value;
-      duration = this.options.duration;
-      this.oldValue = this.value;
-      animationStep = (function(_this) {
-        return function() {
-          var timeDifference;
-          time += 10;
-          timeDifference = (new Date().getTime() - startTime) - time;
-          _this.setValue(Math.round(_this.options.easing(null, time, _this.oldValue, difference, duration)));
-          if (time < _this.options.duration) {
-            return _this.timeout = setTimeout(animationStep, 10 - timeDifference);
+  })(this, function(root, exports, $) {
+    var Countdown;
+    $.fn.extend({
+      countdown: function() {
+        var args, options;
+        options = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+        return this.each(function() {
+          var countdown;
+          countdown = $(this).data('countdown');
+          if (countdown) {
+            if (countdown[options]) {
+              return countdown[options].apply(countdown, args);
+            }
+          } else {
+            return $(this).data('countdown', new Countdown(this, options));
           }
-          return _this.jq_elem.trigger('countdown:finished', {
-            old_value: _this.oldValue,
-            value: _this.value
-          });
-        };
-      })(this);
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-      }
-      return this.timeout = setTimeout(animationStep, 10);
-    };
-
-    Countdown.prototype.stop = function() {
-      if (this.timeout) {
-        clearTimeout(this.timeout);
-        this.jq_elem.trigger('countdown:stopped', {
-          old_value: this.oldValue,
-          value: this.value
         });
-        return delete this.timeout;
       }
-    };
+    });
+    return Countdown = (function() {
+      Countdown.defaults = {
+        placeholder: '-',
+        duration: 1000,
+        digits: 8,
+        min: null,
+        max: null,
+        easing: function(x, t, b, c, d) {
+          return c * t / d + b;
+        }
+      };
 
-    Countdown.prototype.setValue = function(value) {
-      this.value = value;
-      value = value.toString().lpad('0', this.options.digits).split('');
-      return $('.digit', this.jq_elem).each((function(_this) {
-        return function(index, digit) {
-          return $(digit).text(value[index]);
-        };
-      })(this));
-    };
+      function Countdown(elem, options) {
+        this.jq_elem = $(elem);
+        this.value = 0;
+        this.options = $.extend({}, Countdown.defaults, this.jq_elem.data(), options);
+        this.buildHtml();
+        this.parseOptions();
+      }
 
-    Countdown.prototype.setOption = function(option, value) {
-      return this.options[option] = value;
-    };
+      Countdown.prototype.parseOptions = function() {
+        if (this.options.number != null) {
+          this.setValue(this.options.number);
+        }
+        if (this.options.animateTo != null) {
+          return this.animateTo(this.options.animateTo);
+        }
+      };
 
-    return Countdown;
+      Countdown.prototype.buildHtml = function() {
+        var digits, i;
+        i = 0;
+        digits = [];
+        while (i < this.options.digits) {
+          digits.push("<span class=\"digit digit-" + i + "\">" + this.options.placeholder + "</span>");
+          i++;
+        }
+        return this.jq_elem.append(digits.join("\n"));
+      };
 
-  })();
+      Countdown.prototype.animateTo = function(value) {
+        var animationStep, difference, duration, startTime, time;
+        startTime = new Date().getTime();
+        time = 0;
+        if (typeof value === 'string') {
+          if (/^\d+$/.test(value)) {
+            this.targetValue = value;
+          } else {
+            this.targetValue = (this.targetValue || 0) + eval(value);
+          }
+        } else {
+          this.targetValue = value;
+        }
+        if ((this.options.max != null) && this.targetValue > this.options.max) {
+          this.jq_elem.trigger('countdown:limit', {
+            value: this.targetValue,
+            limit: 'max',
+            limited_to: this.options.max
+          });
+          this.targetValue = this.options.max;
+        }
+        if ((this.options.min != null) && this.targetValue < this.options.min) {
+          this.jq_elem.trigger('countdown:limit', {
+            value: this.targetValue,
+            limit: 'min',
+            limited_to: this.options.min
+          });
+          this.targetValue = this.options.min;
+        }
+        difference = this.targetValue - this.value;
+        duration = this.options.duration;
+        this.oldValue = this.value;
+        animationStep = (function(_this) {
+          return function() {
+            var timeDifference;
+            time += 10;
+            timeDifference = (new Date().getTime() - startTime) - time;
+            _this.setValue(Math.round(_this.options.easing(null, time, _this.oldValue, difference, duration)));
+            if (time < _this.options.duration) {
+              return _this.timeout = setTimeout(animationStep, 10 - timeDifference);
+            }
+            return _this.jq_elem.trigger('countdown:finished', {
+              old_value: _this.oldValue,
+              value: _this.value
+            });
+          };
+        })(this);
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+        }
+        return this.timeout = setTimeout(animationStep, 10);
+      };
+
+      Countdown.prototype.stop = function() {
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+          this.jq_elem.trigger('countdown:stopped', {
+            old_value: this.oldValue,
+            value: this.value
+          });
+          return delete this.timeout;
+        }
+      };
+
+      Countdown.prototype.setValue = function(value) {
+        this.value = value;
+        value = value.toString().lpad('0', this.options.digits).split('');
+        return $('.digit', this.jq_elem).each((function(_this) {
+          return function(index, digit) {
+            return $(digit).text(value[index]);
+          };
+        })(this));
+      };
+
+      Countdown.prototype.setOption = function(option, value) {
+        return this.options[option] = value;
+      };
+
+      return Countdown;
+
+    })();
+  });
 
   if (typeof String.prototype.lpad !== 'function') {
     String.prototype.lpad = function(padString, length) {
--- src/countdown.coffee
+++ src/countdown.coffee
@@ -1,93 +1,109 @@
-$.fn.extend 
-  countdown: (options, args...) ->
-    this.each ->
-      countdown = $(this).data 'countdown'
-      if countdown
-        countdown[options].apply(countdown, args) if countdown[options]
-      else
-        $(this).data 'countdown', new Countdown( this, options )
+((root, factory) ->
 
-class Countdown
-  
-  @defaults = 
-    placeholder: '-'
-    duration: 1000
-    digits: 8
-    min: null
-    max: null
-    easing: (x, t, b, c, d) ->
-      # Default linear interpolation
-      c*t/d + b;
-  
-  constructor: (elem, options) ->
-    @jq_elem = $ elem
-    @value = 0
-    @options = $.extend({}, Countdown.defaults, @jq_elem.data(), options)
-    this.buildHtml()
-    this.parseOptions()
-    
-  parseOptions: ->
-    this.setValue(@options.number) if @options.number?
-    this.animateTo(@options.animateTo) if @options.animateTo?
-  
-  buildHtml: ->
-    i = 0
-    digits = []
-    while i < @options.digits
-      digits.push "<span class=\"digit digit-#{i}\">#{@options.placeholder}</span>"
-      i++
-    @jq_elem.append digits.join("\n")
-  
-  animateTo: (value) ->
-    startTime = new Date().getTime()
-    time = 0
-    if typeof value == 'string'
-      if /^\d+$/.test(value)
+  # AMD
+  if typeof define is 'function' && define.amd
+    define ['jquery', 'exports'], ($, exports) ->
+      factory(root, exports, $)
+
+  # CommonJS
+  else if typeof exports != 'undefined'
+    factory(root, exports, require('jquery'))
+
+  # Browser
+  else
+    factory(root, {}, (root.jQuery || root.$))
+
+) this, (root, exports, $) ->
+
+  $.fn.extend
+    countdown: (options, args...) ->
+      this.each ->
+        countdown = $(this).data 'countdown'
+        if countdown
+          countdown[options].apply(countdown, args) if countdown[options]
+        else
+          $(this).data 'countdown', new Countdown( this, options )
+
+  class Countdown
+
+    @defaults =
+      placeholder: '-'
+      duration: 1000
+      digits: 8
+      min: null
+      max: null
+      easing: (x, t, b, c, d) ->
+        # Default linear interpolation
+        c * t / d + b;
+
+    constructor: (elem, options) ->
+      @jq_elem = $ elem
+      @value = 0
+      @options = $.extend({}, Countdown.defaults, @jq_elem.data(), options)
+      this.buildHtml()
+      this.parseOptions()
+
+    parseOptions: ->
+      this.setValue(@options.number) if @options.number?
+      this.animateTo(@options.animateTo) if @options.animateTo?
+
+    buildHtml: ->
+      i = 0
+      digits = []
+      while i < @options.digits
+        digits.push "<span class=\"digit digit-#{i}\">#{@options.placeholder}</span>"
+        i++
+      @jq_elem.append digits.join("\n")
+
+    animateTo: (value) ->
+      startTime = new Date().getTime()
+      time = 0
+      if typeof value == 'string'
+        if /^\d+$/.test(value)
+          @targetValue = value
+        else
+          @targetValue = (@targetValue || 0) + eval(value)
+      else 
         @targetValue = value
-      else
-        @targetValue = (@targetValue || 0) + eval(value)
-    else 
-      @targetValue = value
-    
-    if @options.max? and @targetValue > @options.max
-      @jq_elem.trigger 'countdown:limit', {value: @targetValue, limit: 'max', limited_to: @options.max}
-      @targetValue = @options.max
-    
-    if @options.min? and @targetValue < @options.min
-      @jq_elem.trigger 'countdown:limit', {value: @targetValue, limit: 'min', limited_to: @options.min}
-      @targetValue = @options.min
-    
-    difference = @targetValue - @value
-    duration = @options.duration
-    @oldValue = @value
-    
-    animationStep = =>
-      time += 10 
-      timeDifference = (new Date().getTime() - startTime) - time
-      this.setValue Math.round( @options.easing(null, time, @oldValue, difference, duration) )
-      if time < @options.duration
-        return @timeout = setTimeout animationStep, 10 - timeDifference
-      @jq_elem.trigger 'countdown:finished', {old_value: @oldValue, value: @value}
-    
-    clearTimeout @timeout if @timeout
-    @timeout = setTimeout animationStep, 10
-  
-  stop: ->
-    if @timeout
-      clearTimeout @timeout
-      @jq_elem.trigger 'countdown:stopped', {old_value: @oldValue, value: @value}
-      delete @timeout
-  
-  setValue: (value) ->
-    @value = value 
-    value = value.toString().lpad('0', @options.digits).split('')
-    $('.digit', @jq_elem).each (index, digit) =>
-      $(digit).text value[index]
-  
-  setOption: (option, value) ->
-    @options[option] = value
-  
-  
+
+      if @options.max? and @targetValue > @options.max
+        @jq_elem.trigger 'countdown:limit', {value: @targetValue, limit: 'max', limited_to: @options.max}
+        @targetValue = @options.max
+
+      if @options.min? and @targetValue < @options.min
+        @jq_elem.trigger 'countdown:limit', {value: @targetValue, limit: 'min', limited_to: @options.min}
+        @targetValue = @options.min
+
+      difference = @targetValue - @value
+      duration = @options.duration
+      @oldValue = @value
+
+      animationStep = =>
+        time += 10 
+        timeDifference = (new Date().getTime() - startTime) - time
+        this.setValue Math.round( @options.easing(null, time, @oldValue, difference, duration) )
+        if time < @options.duration
+          return @timeout = setTimeout animationStep, 10 - timeDifference
+        @jq_elem.trigger 'countdown:finished', {old_value: @oldValue, value: @value}
+
+      clearTimeout @timeout if @timeout
+      @timeout = setTimeout animationStep, 10
+
+    stop: ->
+      if @timeout
+        clearTimeout @timeout
+        @jq_elem.trigger 'countdown:stopped', {old_value: @oldValue, value: @value}
+        delete @timeout
+
+    setValue: (value) ->
+      @value = value
+      value = value.toString().lpad('0', @options.digits).split('')
+      $('.digit', @jq_elem).each (index, digit) =>
+        $(digit).text value[index]
+
+    setOption: (option, value) ->
+      @options[option] = value
+
 unless typeof String::lpad == 'function'
   String::lpad = (padString, length) ->
     str = this
